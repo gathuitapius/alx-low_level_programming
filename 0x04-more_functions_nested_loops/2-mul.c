b62f7985002d609e8361f4dabe1ac011883e18d9
gathuitapius <gathuitapius097@gmail.com>
2023-03-27 04:49:33 -0700

    more functions and nested loops commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @num1: int to be multiplied to num2
+ * @num2: int to be multiplied to num1
+ * Return: the result of the operation
+ */
+int mul(int num1, int num2)
+{
+	int result;
+
+	result = num1 * num2;
+
+	return (result);
+}
